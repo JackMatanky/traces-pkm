ec7d4820387f93a604f6571a96f212cdc442544e
Jack Matanky <JackMatanky@gmail.com>
2026-08-21 11:12:07 +0300

    chore: clippy fix

--- src/template/engine/query.rs
+++ src/template/engine/query.rs
@@ -517,7 +517,7 @@ impl Object for QueryRecord {
     }
 
     fn custom_cmp(self: &Arc<Self>, other: &DynObject) -> Option<Ordering> {
-        other.downcast_ref::<QueryRecord>().map(|other| {
+        other.downcast_ref::<Self>().map(|other| {
             if **self == *other {
                 Ordering::Equal
             } else {
